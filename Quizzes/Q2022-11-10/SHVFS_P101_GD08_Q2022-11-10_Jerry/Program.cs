1c283150bedef150a61e187f0418ed7c5e92d39d
ku202240 <115707980+ku202240@users.noreply.github.com>
2022-11-11 17:00:18 +0800

    111

---
+++ Quizzes/Q2022-11-10/SHVFS_P101_GD08_Q2022-11-10_Jerry/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SHVFS_P101_GD08_Jerry
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string a, b;
+            string[] names = { "Jerry", "Oli", "Navin", "People", "Shift", "Maya" ,"Tom" };
+            Random rand = new Random();
+            int name = rand.Next(names.Length);
+            Console.WriteLine("hey,what your name?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("ok,Let's guess the words together");
+            b = Console.ReadLine();
+            while   (b != names[name])
+            {
+                Console.WriteLine("Hee hee, let's guess the value");
+                b = Console.ReadLine();
+                Console.Clear();
+            }
+            
+            //if (b == names[name])
+            Console.WriteLine("You guessed it");
+
+        }
+    }
+}
